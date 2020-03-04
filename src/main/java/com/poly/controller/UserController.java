032df38252a85c3b1e0cc5cb7d70a07f83e30e9a
Thinhtlt <tranlethanhthinh@gmail.com>
2020-03-04 23:30:08 +0700

    /cm

--- src/main/java/com/poly/controller/UserController.java
+++ src/main/java/com/poly/controller/UserController.java
@@ -1,10 +1,13 @@
 package com.poly.controller;
 
 import java.io.File;
+import java.text.SimpleDateFormat;
+import java.util.Date;
 import java.util.List;
 
 import javax.servlet.ServletContext;
 import javax.servlet.http.HttpSession;
+import javax.validation.Valid;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
@@ -21,10 +24,12 @@ import org.springframework.web.multipart.MultipartFile;
 import com.poly.dao.CategoryDAO;
 import com.poly.dao.OrderDetailDAO;
 import com.poly.dao.ProductDAO;
+import com.poly.dao.ReviewDAO;
 import com.poly.dao.RoleDAO;
 import com.poly.dao.UserDAO;
 import com.poly.entity.Category;
 import com.poly.entity.Product;
+import com.poly.entity.Review;
 import com.poly.entity.Role;
 import com.poly.entity.User;
 
@@ -48,6 +53,9 @@ public class UserController {
 	
 	@Autowired
 	OrderDetailDAO orderDetailDao;
+	
+	@Autowired
+	ReviewDAO reviewDAO;
 
 	@Autowired
 	RoleDAO roleDAO;
@@ -107,6 +115,8 @@ public class UserController {
 	public String confirmation() {
 		return "user/confirmation";
 	}
+	
+
 
 	@GetMapping("/user/blog")
 	public String blog() {
@@ -175,5 +185,50 @@ public class UserController {
 
 //		model.addAttribute("form" , user);
 		return "user/login";
+	}
+	@GetMapping("/user/createNews")
+	public String createNews(Model model) {
+		model.addAttribute("news", new Review());
+		// tao object moi
+		return "user/createNews";
+	}
+	
+	@PostMapping("/user/createNews")
+	public String register(Model model,@Valid @ModelAttribute("news") Review review,  BindingResult errors ,
+			@RequestParam("up_photo") MultipartFile file) {
+		if (file.isEmpty()) {
+			review.setThumbnail("news.png");
+		} else {
+			review.setThumbnail(file.getOriginalFilename());
+			try {
+				String path = app.getRealPath("/static/user/news/" + review.getThumbnail());
+				file.transferTo(new File(path));
+			} catch (Exception e) {
+				e.printStackTrace();
+			}
+		}
+		if (errors.hasErrors()) {
+			model.addAttribute("message", "Vui lòng sửa các lỗi sau đây");
+
+			return "user/createNews";	
+		}else {
+			try {
+				User user = (User) session.getAttribute("user");// lay cai thang login vao
+				review.setUser(user);// set cai thang do vao 
+				review.setStatus(false);
+				review.setCreateDate(new Date());
+				review.setCountViewer(0);
+				reviewDAO.create(review);
+				model.addAttribute("message", "Tạo bài viết thành công!");
+			} catch (Exception e) {
+				model.addAttribute("message", "Tạo bài viết  thất bại!");
+				
+			}
+		}
+		
+
+		// model.addAttribute("form" , user);
+
+		return "redirect:/user/management";
 	}
 }
---
+++ src/main/java/com/poly/entity/Review.java
@@ -0,0 +1,126 @@
+package com.poly.entity;
+
+import java.util.Date;
+import java.util.List;
+
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.JoinColumn;
+import javax.persistence.ManyToOne;
+import javax.persistence.OneToMany;
+import javax.persistence.Table;
+@Entity
+@Table(name="Reviews")
+public class Review {
+	@Id
+	@GeneratedValue(strategy=GenerationType.IDENTITY)
+	Integer id;
+	/*
+	 * @NotBlank(message = "Không được để trống title")// phải là chuỗi String
+	 * 
+	 * @Length(min=6 , message = "title name phải có ít nhất 6 ký tự")
+	 */
+	String title;
+	/*
+	 * @NotBlank(message = "Không được để trống description")// phải là chuỗi String
+	 * 
+	 * @Length(min=6 , message = "description name phải có ít nhất 6 ký tự")
+	 */
+	String description;
+	/* @NotBlank(message = "Không được để trống content") */
+	String content;
+	String thumbnail;	
+//	String CreateBy;	
+	/* @Temporal(TemporalType.DATE) */
+	Date createDate;
+	Boolean status;
+//	Integer CategoryId;
+	Integer countViewer;
+
+	
+	@ManyToOne
+	@JoinColumn(name="createBy")
+	User user;
+
+	public Integer getId() {
+		return id;
+	}
+
+	public Date getCreateDate() {
+		return createDate;
+	}
+
+	public void setCreateDate(Date createDate) {
+		this.createDate = createDate;
+	}
+
+	public void setId(Integer id) {
+		this.id = id;
+	}
+
+	public String getTitle() {
+		return title;
+	}
+
+	public void setTitle(String title) {
+		this.title = title;
+	}
+
+	public String getDescription() {
+		return description;
+	}
+
+	public void setDescription(String description) {
+		this.description = description;
+	}
+
+	
+
+	public String getThumbnail() {
+		return thumbnail;
+	}
+
+	public void setThumbnail(String thumbnail) {
+		this.thumbnail = thumbnail;
+	}
+
+
+	public Boolean getStatus() {
+		return status;
+	}
+
+	public void setStatus(Boolean status) {
+		this.status = status;
+	}
+
+
+	public String getContent() {
+		return content;
+	}
+
+	public void setContent(String content) {
+		this.content = content;
+	}
+
+	public Integer getCountViewer() {
+		return countViewer;
+	}
+
+	public void setCountViewer(Integer countViewer) {
+		this.countViewer = countViewer;
+	}
+
+	public User getUser() {
+		return user;
+	}
+
+	public void setUser(User user) {
+		this.user = user;
+	}
+
+
+
+
+}
