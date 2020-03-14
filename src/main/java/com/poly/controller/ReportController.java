3217923f1ff2141d18230f688a5b967b5e186c35
Dautumn <48059156+Dautumn@users.noreply.github.com>
2020-03-14 18:07:20 +0700

    Report bên admin
    Report cho admin

---
+++ src/main/java/com/poly/controller/ReportController.java
@@ -0,0 +1,19 @@
+package com.poly.controller;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+import com.poly.dao.ReportDAO;
+
+@Controller
+public class ReportController {
+	@Autowired
+	ReportDAO dao;
+	@RequestMapping("admin/Report")
+	public String inventoryByCategory(Model model) {
+		model.addAttribute("data",dao.inventoryByCategory());
+		return "admin/Report";
+	}
+}
